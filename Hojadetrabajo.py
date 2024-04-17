5f2674c5f3d33bcf362dff1b41073d6612f7c4b2
ErwinJ16 <158303409+ErwinJ16@users.noreply.github.com>
2024-04-17 11:07:07 -0600

    Add files via upload

---
+++ Hojadetrabajo.py
@@ -0,0 +1,12 @@
+#Ejercicio1
+def saludar():
+    print("Hola, bienvenido a python") 
+    pass
+saludar()
+
+#Ejercicio2
+def sumar(a,b):
+    return a + b
+
+resultado=sumar(5,3)
+print(resultado)
