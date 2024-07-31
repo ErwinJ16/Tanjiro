5a1260d38dcbfe99c2a41a4ea0cb6a20e09b301a
ErwinJ16 <158303409+ErwinJ16@users.noreply.github.com>
2024-07-31 10:53:39 -0600

    Update Juego.py

--- Juego.py
+++ Juego.py
@@ -1,267 +1,126 @@
 import pygame
 import sys
+
+states = {
+    "Pantalla inicial": "inicio.jpg",
+    "Pantalla 1": "instrucciones.png",
+    "Pantalla 2": "opciones.png",
+    "Pantalla 3": "imagen1.png",
+    "Pantalla 4": "imagen2.png",
+    "Pantalla 5": "texto1.png",
+    "Pantalla 6": "imagen 3.png",
+    "Pantalla 7": "imagen 4.png",
+    "Pantalla 8": "texto 2.png",
+    "Pantalla 9": "texto 3.png",
+    "Pantalla 10": "texto 4.png",
+    "Pantalla 11": "pueblo 1.png",
+    "Pantalla 12": "pueblo 2.png",
+    "Pantalla 13": "texto 5.png",
+    "Pantalla 14": "texto 6.png",
+    "Pantalla 15": "prometida.png",
+    "Pantalla 16": "texto7.png",
+    "Pantalla 17": "pueblo 3.png",
+    "Pantalla 18": "pergaminono.png",
+    "Pantalla 19": "caminar.png",
+    "Pantalla 20": "texto8.png",
+    "Pantalla 21": "piedras.png",
+    "Pantalla 22": "texto9.png",
+    "Pantalla 23": "inicio3.png",
+    "Pantalla 24": "demonio.png",
+    "Pantalla 25": "muerte1.png",
+    "Pantalla 26": "restos.png",
+    "Pantalla 27": "dialogo.png",
+    "Pantalla 28": "manus.png",
+    "Pantalla 29": "origen.png",
+    "Pantalla 30": "llegar.png",
+    "Pantalla 31": "camino.png",
+    "Pantalla 32": "destino.png",
+    "Pantalla 33": "boss.png",
+    "Pantalla 34": "boss2.png",
+    "Pantalla 35": "boss3.png",
+    "Pantalla 36": "convertir.png",
+    "Pantalla 37": "convertir2.png",
+    "Pantalla 38": "convertir3.png",
+    "Pantalla 39": "convertir4.png",
+    "Pantalla 40": "poderno.png",
+    "Pantalla 41": "heroe.png",
+    "Pantalla 42": "heroe2.png",
+    "Pantalla 43": "heroe3.png",
+    "Pantalla 44": "heroe4.png",
+    "Pantalla 45": "heroe5.png"
+}
+
+transitions = {
+    "Pantalla inicial": {"space": "Pantalla 1"},
+    "Pantalla 1": {"space": "Pantalla 2"},
+    "Pantalla 2": {"left": "Pantalla 3", "right": "Pantalla 4", "space": "Pantalla 5"},
+    "Pantalla 3": {},
+    "Pantalla 4": {"space": "Pantalla 28"},
+    "Pantalla 5": {"left": "Pantalla 6", "right": "Pantalla 7"},
+    "Pantalla 6": {},
+    "Pantalla 7": {},
+    "Pantalla 8": {"space": "Pantalla 9"},
+    "Pantalla 9": {"left": "Pantalla 24", "right": "Pantalla 27"},
+    "Pantalla 10": {"left": "Pantalla 11", "right": "Pantalla 18"},
+    "Pantalla 11": {"space": "Pantalla 12"},
+    "Pantalla 12": {"space": "Pantalla 13"},
+    "Pantalla 13": {"space": "Pantalla 14"},
+    "Pantalla 14": {"space": "Pantalla 15"},
+    "Pantalla 15": {"space": "Pantalla 16"},
+    "Pantalla 16": {"space": "Pantalla 17"},
+    "Pantalla 17": {},
+    "Pantalla 18": {},
+    "Pantalla 19": {"space": "Pantalla 20"},
+    "Pantalla 20": {"space": "Pantalla 21"},
+    "Pantalla 21": {"space": "Pantalla 22"},
+    "Pantalla 22": {"space": "Pantalla 23"},
+    "Pantalla 23": {},
+    "Pantalla 24": {"space": "Pantalla 25"},
+    "Pantalla 25": {"space": "Pantalla 26"},
+    "Pantalla 26": {"space": "Pantalla 27"},
+    "Pantalla 27": {"space": "Pantalla 19"},
+    "Pantalla 28": {"left": "Pantalla 29", "right": "Pantalla 30", "space": "Pantalla 4"},
+    "Pantalla 29": {"space": "Pantalla 31"},
+    "Pantalla 30": {"space": "Pantalla 31"},
+    "Pantalla 31": {"space": "Pantalla 32"},
+    "Pantalla 32": {"left": "Pantalla 33", "right": "Pantalla 41", "space": "Pantalla 35"},
+    "Pantalla 33": {"space": "Pantalla 34"},
+    "Pantalla 34": {"space": "Pantalla 35"},
+    "Pantalla 35": {"left": "Pantalla 36", "right": "Pantalla 40", "space": "Pantalla 37"},
+    "Pantalla 36": {"space": "Pantalla 37"},
+    "Pantalla 37": {"space": "Pantalla 38"},
+    "Pantalla 38": {"space": "Pantalla 39"},
+    "Pantalla 39": {"space": "Pantalla 40"},
+    "Pantalla 40": {"space": "Pantalla 19"},
+    "Pantalla 41": {"space": "Pantalla 42"},
+    "Pantalla 42": {"left": "Pantalla 43", "right": "Pantalla 44", "space": "Pantalla 45"},
+    "Pantalla 43": {"space": "Pantalla 19"},
+    "Pantalla 44": {"space": "Pantalla 45"},
+    "Pantalla 45": {}
+}
+
 pygame.init()
-
-
 screen_width = 1200
 screen_height = 900
 screen = pygame.display.set_mode((screen_width, screen_height))
+estado = "Pantalla inicial"
 
-estado="Pantalla inicial"
-
-def cargar(nombre):
-    img=pygame.transform.scale(pygame.image.load(nombre), (screen_width, screen_height))
-    screen.blit(img, (0, 0))
-    pygame.display.update()
-
-running = True
-while running:
+while True:
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            running=False
-        if event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_SPACE:
-                print(estado)
-                if estado == "Pantalla inicial":
-                    estado = "Pantalla 1"
-                elif estado == "Pantalla 1":
-                    estado = "Pantalla 2"
-                elif estado == "Pantalla 3":
-                    estado="Pantalla 5"
-                elif estado=="Pantalla 6":
-                    estado="Pantalla 8" 
-                elif estado=="Pantalla 7":
-                    estado="Pantalla 9" 
-                elif estado=="Pantalla 8":
-                    estado="Pantalla 10"
-                elif estado=="Pantalla 11":  
-                    estado="Pantalla 12"
-                elif estado=="Pantalla 12":  
-                    estado="Pantalla 13"
-                elif estado=="Pantalla 13":
-                    estado="Pantalla 14"
-                elif estado=="Pantalla 14":
-                    estado="Pantalla 15"
-                elif estado=="Pantalla 15":
-                    estado="Pantalla 16"
-                elif estado=="Pantalla 16":
-                    estado="Pantalla 17"
-                elif estado=="Pantalla 18":
-                    estado="Pantalla 19"
-                elif estado=="Pantalla 19":
-                    estado="Pantalla 20"
-                elif estado=="Pantalla 20":
-                    estado="Pantalla 21" 
-                elif estado=="Pantalla 21":
-                    estado="Pantalla 22" 
-                elif estado=="Pantalla 22":
-                    estado="Pantalla 23" 
-                elif estado=="Pantalla 24":
-                    estado="Pantalla 25"
-                elif estado=="Pantalla 25":
-                    estado="Pantalla 26"
-                elif estado=="Pantalla 27":
-                    estado="Pantalla 19"
-                elif estado=="Pantalla 4":
-                    estado="Pantalla 28"
-                elif estado=="Pantalla 29":
-                    estado="Pantalla 31"
-                elif estado=="Pantalla 30":
-                    estado="Pantalla 31"
-                elif estado=="Pantalla 31":
-                    estado="Pantalla 32"
-                elif estado=="Pantalla 33":
-                    estado="Pantalla 34"
-                elif estado=="Pantalla 34":
-                    estado="Pantalla 35"
-                elif estado=="Pantalla 36":
-                    estado="Pantalla 37"
-                elif estado=="Pantalla 37":
-                    estado="Pantalla 38"
-                elif estado=="Pantalla 38":
-                    estado="Pantalla 39"
-                elif estado=="Pantalla 40":
-                    estado="Pantalla 19"
-                elif estado=="Pantalla 41":
-                    estado="Pantalla 42"
-                elif estado=="Pantalla 43":
-                    estado="Pantalla 19"
-                elif estado=="Pantalla 44":
-                    estado="Pantalla 45"
-            if event.key == pygame.K_LEFT:
-                if estado == "Pantalla 2":
-                    estado = "Pantalla 3"
-                elif estado=="Pantalla 5":
-                    estado="Pantalla 6"
-                elif estado=="Pantalla 10":
-                    estado="Pantalla 11"
-                elif estado=="Pantalla 9":
-                    estado="Pantalla 24"
-                elif estado=="Pantalla 28":
-                    estado="Pantalla 29"   
-                elif estado=="Pantalla 32":
-                    estado="Pantalla 33"  
-                elif estado=="Pantalla 35":
-                    estado="Pantalla 36" 
-                elif estado=="Pantalla 42":
-                    estado="Pantalla 43"
-            if event.key==pygame.K_RIGHT:
-                if estado=="Pantalla 5":
-                    estado="Pantalla 7"
-                elif estado=="Pantalla 5":
-                    estado="Pantalla 26"  
-                elif estado=="Pantalla 10":
-                    estado="Pantalla 18"
-                elif estado=="Pantalla 9":
-                    estado="Pantalla 27" 
-                elif estado=="Pantalla 28":
-                    estado="Pantalla 30" 
-                elif estado=="Pantalla 32":
-                    estado="Pantalla 41" 
-                elif estado=="Pantalla 35":
-                    estado="Pantalla 40" 
-                elif estado=="Pantalla 42":
-                    estado="Pantalla 44"  
-                elif estado == "Pantalla 2":
-                    estado = "Pantalla 4"                
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                estado = transitions[estado].get("space", estado)
+            elif event.key == pygame.K_LEFT:
+                estado = transitions[estado].get("left", estado)
+            elif event.key == pygame.K_RIGHT:
+                estado = transitions[estado].get("right", estado)
 
-    if estado=="Pantalla inicial":
-        cargar("inicio.jpg")
-
-    elif estado=="Pantalla 1":
-        cargar("instrucciones.png")
-
-    elif estado=="Pantalla 2":
-        cargar("opciones.png")
-
-    elif estado=="Pantalla 3":
-        cargar ("imagen1.png")
-
-    elif estado=="Pantalla 4":
-        cargar("imagen2.png")
-
-    elif estado=="Pantalla 5":
-        cargar("texto1.png")
-
-    elif estado=="Pantalla 6":
-        cargar ("imagen 3.png")    
-
-    elif estado=="Pantalla 7":
-        cargar ("imagen 4.png")
-
-    elif estado=="Pantalla 8":
-        cargar ("texto 2.png")    
-
-    elif estado=="Pantalla 9":
-        cargar ("texto 3.png")
-    
-    elif estado=="Pantalla 10":
-        cargar ("texto 4.png") 
-
-    elif estado=="Pantalla 11":
-        cargar ("pueblo 1.png")
-    
-    elif estado=="Pantalla 12":
-        cargar ("pueblo 2.png")
-    
-    elif estado=="Pantalla 13":
-        cargar ("texto 5.png")
-    
-    elif estado=="Pantalla 14":
-        cargar ("texto 6.png")
-
-    elif estado=="Pantalla 15":
-        cargar ("prometida.png")
-    
-    elif estado=="Pantalla 16":
-        cargar ("texto7.png")
-
-    elif estado=="Pantalla 17":
-        cargar ("pueblo 3.png")
-    
-    elif estado=="Pantalla 18":
-        cargar ("pergaminono.png")
-    
-    elif estado=="Pantalla 19":
-        cargar ("caminar.png")
-
-    elif estado=="Pantalla 20":
-        cargar ("texto8.png")
-
-    elif estado=="Pantalla 21":
-        cargar ("piedras.png")
-
-    elif estado=="Pantalla 22":
-        cargar ("texto9.png")
-
-    elif estado=="Pantalla 23":
-        cargar ("inicio3.png")
-
-    elif estado=="Pantalla 24":
-        cargar ("demonio.png") 
-
-    elif estado=="Pantalla 25":
-        cargar ("muerte1.png")
-
-    elif estado=="Pantalla 26":
-        cargar ("restos.png")  
-
-    elif estado=="Pantalla 27":
-        cargar ("dialogo.png")
-
-    elif estado=="Pantalla 28":
-        cargar ("manus.png")
-    
-    elif estado=="Pantalla 29":
-        cargar ("origen.png")
-    
-    elif estado=="Pantalla 30":
-        cargar ("llegar.png")
-    
-    elif estado=="Pantalla 31":
-        cargar ("camino.png")
-    
-    elif estado=="Pantalla 32":
-        cargar ("destino.png")
-       
-    elif estado=="Pantalla 33":
-        cargar ("boss.png")
-    
-    elif estado=="Pantalla 34":
-        cargar ("boss2.png") 
-    
-    elif estado=="Pantalla 35":
-        cargar ("boss3.png") 
-    
-    elif estado=="Pantalla 36":
-        cargar ("convertir.png")
-
-    elif estado=="Pantalla 37":
-        cargar ("convertir2.png")
-
-    elif estado=="Pantalla 38":
-        cargar ("convertir3.png")
-
-    elif estado=="Pantalla 39":
-        cargar ("convertir4.png")
-
-    elif estado=="Pantalla 40":
-        cargar ("poderno.png")
-
-    elif estado=="Pantalla 41":
-        cargar ("heroe.png")
-    
-    elif estado=="Pantalla 42":
-        cargar ("heroe2.png")
-    
-    elif estado=="Pantalla 43":
-        cargar ("heroe3.png")
-
-    elif estado=="Pantalla 44":
-        cargar ("heroe4.png")
-
-    elif estado=="Pantalla 45":
-        cargar ("heroe5.png")
+    image = pygame.image.load(states[estado])
+    image = pygame.transform.scale(image, (screen_width, screen_height))
+    screen.blit(image, (0, 0))
     pygame.display.flip()
 
 pygame.quit()
